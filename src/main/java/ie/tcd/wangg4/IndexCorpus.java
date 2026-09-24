47c2262b41b97df559f2144ed6d31204c27d42b0
Guangchen Wang <guangchenwa@gmail.com>
2026-09-24 14:19:53 +0100

    Standardized indentation to space

--- src/main/java/ie/tcd/wangg4/IndexCorpus.java
+++ src/main/java/ie/tcd/wangg4/IndexCorpus.java
@@ -14,39 +14,39 @@ import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.store.Directory;
  
 public class IndexCorpus {
-	public static void main(String[] args) throws IOException {
-		// validate arguments
-		if (args.length <= 0) {
+    public static void main(String[] args) throws IOException {
+        // validate arguments
+        if (args.length <= 0) {
             System.out.println("Expected corpus as input");
             System.exit(1);            
         }
 
-		// Analyzer that is used to process TextField
-		Analyzer analyzer = new StandardAnalyzer();
-		
-		// List for pooling documents in corpus
-		ArrayList<Document> documents = new ArrayList<Document>();
+        // Analyzer that is used to process TextField
+        Analyzer analyzer = new StandardAnalyzer();
+        
+        // List for pooling documents in corpus
+        ArrayList<Document> documents = new ArrayList<Document>();
 
-		for (Path file : Common.collectFiles(args)) {
-			System.out.printf("Indexing \"%s\"\n", file);
-			// Create a new document and add the file's contents
-			Document doc = new Document();
-			// Parse file content
-			String content = Common.readFile(file);
-			doc.add(new StringField("filename", file.toString(), Field.Store.YES));
-			doc.add(new TextField("content", content, Field.Store.YES));
-			// Add the file to list
-			documents.add(doc);
-		}
+        for (Path file : Common.collectFiles(args)) {
+            System.out.printf("Indexing \"%s\"\n", file);
+            // Create a new document and add the file's contents
+            Document doc = new Document();
+            // Parse file content
+            String content = Common.readFile(file);
+            doc.add(new StringField("filename", file.toString(), Field.Store.YES));
+            doc.add(new TextField("content", content, Field.Store.YES));
+            // Add the file to list
+            documents.add(doc);
+        }
 
-		// Create the IndexWriter
-		Directory directory = Common.openIndexDirectory();
-		IndexWriter iwriter = Common.createIndexWriter(directory, analyzer);
-		// Write all the documents in the linked list to the search index
-		iwriter.addDocuments(documents);
+        // Create the IndexWriter
+        Directory directory = Common.openIndexDirectory();
+        IndexWriter iwriter = Common.createIndexWriter(directory, analyzer);
+        // Write all the documents in the linked list to the search index
+        iwriter.addDocuments(documents);
 
-		// Commit everything and close
-		iwriter.close();
-		directory.close();
-	}
+        // Commit everything and close
+        iwriter.close();
+        directory.close();
+    }
 }
--- src/main/java/ie/tcd/wangg4/QueryIndex2.java
+++ src/main/java/ie/tcd/wangg4/QueryIndex2.java
@@ -28,63 +28,56 @@ import org.apache.lucene.queryparser.classic.ParseException;
 public class QueryIndex2
 {
 
-	// the location of the search index
-	private static String INDEX_DIRECTORY = "../index";
-	
-	// Limit the number of search results we get
-	private static int MAX_RESULTS = 10;
-	
-	public static void main(String[] args) throws IOException, ParseException
-	{
-		// Analyzer used by the query parser.
-		// Must be the same as the one used when creating the index
-		Analyzer analyzer = new StandardAnalyzer();
-		
-		// Open the folder that contains our search index
-		Directory directory = FSDirectory.open(Paths.get(INDEX_DIRECTORY));
-		
-		// create objects to read and search across the index
-		DirectoryReader ireader = DirectoryReader.open(directory);
-		IndexSearcher isearcher = new IndexSearcher(ireader);
-		
-		// Create the query parser. The default search field is "content", but
-		// we can use this to search across any field
-		QueryParser parser = new QueryParser("content", analyzer);
-		
-		String queryString = "";
-		Scanner scanner = new Scanner(System.in);
-		do
-		{
-			// trim leading and trailing whitespace from the query
-			queryString = queryString.trim();
+    // the location of the search index
+    private static String INDEX_DIRECTORY = "../index";
+    
+    // Limit the number of search results we get
+    private static int MAX_RESULTS = 10;
+    
+    public static void main(String[] args) throws IOException, ParseException {
+        // Open the folder that contains our search index
+        Directory directory = Common.openIndexDirectory();
+        
+        // Analyzer used by the query parser.
+        // Must be the same as the one used when creating the index
+        Analyzer analyzer = new StandardAnalyzer();
+        
+        // create objects to read and search across the index
+        DirectoryReader ireader = DirectoryReader.open(directory);
+        IndexSearcher isearcher = new IndexSearcher(ireader);
+        
+        // Create the query parser. The default search field is "content", 
+        // but we can use this to search across any field
+        QueryParser parser = new QueryParser("content", analyzer);
+        
+        String queryString = "";
+        Scanner scanner = new Scanner(System.in);
+        do {
+            // trim leading and trailing whitespace from the query
+            queryString = queryString.trim();
 
-			// if the user entered a querystring
-			if (queryString.length() > 0)
-			{
-				// parse the query with the parser
-				Query query = parser.parse(queryString);
+            // if the user entered a querystring
+            if (queryString.length() > 0)
+            {
+                // parse the query with the parser
+                Query query = parser.parse(queryString);
 
-				// Get the set of results
-				ScoreDoc[] hits = isearcher.search(query, MAX_RESULTS).scoreDocs;
+                // Get the set of results
+                ScoreDoc[] hits = isearcher.search(query, MAX_RESULTS).scoreDocs;
 
-				// Print the results
-				System.out.println("Documents: " + hits.length);
-				for (int i = 0; i < hits.length; i++)
-				{
-					Document hitDoc = isearcher.storedFields().document(hits[i].doc);
-					System.out.println(i + ") " + hitDoc.get("filename") + " " + hits[i].score);
-				}
-
-				System.out.println();	
-			}
-			
-			// prompt the user for input and quit the loop if they escape
-			System.out.print(">>> ");
-			queryString = scanner.nextLine();
-		} while (!queryString.equals("\\q"));
-		
-		// close everything and quit
-		ireader.close();
-		directory.close();
-	}
+                // Print the results
+                Common.printResults(isearcher, hits);
+                System.out.println();	
+            }
+            
+            // prompt the user for input and quit the loop if they escape
+            System.out.print(">>> ");
+            queryString = scanner.nextLine();
+        } while (!queryString.equals("\\q"));
+        
+        // close everything and quit
+        scanner.close();
+        ireader.close();
+        directory.close();
+    }
 }
